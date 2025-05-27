922d66dfbde7d8ed877de0567deb3c9b4c60198f
vk <VKorelsky@users.noreply.github.com>
2025-05-27 15:34:41 -0400

    rejig the folder arrangement to see if it helps with pytest test discovery

---
+++ tests/test_server.py
@@ -0,0 +1,23 @@
+def id():
+    assert 2 == 2
+    
+print("93827491827490123874039128740391287049871320948701239847019238740912837409182730")
+
+# def test_create_circle():
+#     client = socketio.test_client(app)
+#     response = client.get_received()
+#     assert len(response) == 0  # No messages received yet
+    
+#     # Test HTTP endpoint
+#     with app.test_client() as http_client:
+#         response = http_client.post("/circle/new")
+#         assert response.status_code == 200
+#         data = response.get_json()
+#         assert "circle_id" in data
+#         circle_id = uuid.UUID(data["circle_id"])
+        
+#         # Test socket connection to the new circle
+#         client.connect(app, query_string=f"circleId={circle_id}")
+#         response = client.get_received()
+#         assert len(response) == 0  # No messages on connect
+
